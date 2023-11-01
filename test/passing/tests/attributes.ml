4626248646a5da6b7a99d30adad6bb9cf4f27d96
alanechang <alanechang@janestreet.com>
2023-11-01 13:36:18 -0400

    new layout annot on type decl
    Signed-off-by: alanechang <alanechang@janestreet.com>

--- test/passing/tests/attributes.ml
+++ test/passing/tests/attributes.ml
@@ -183,8 +183,7 @@ module type A = sig
 end
 
 module M = struct
-  type t
-  [@@immediate]
+  type t : immediate
   (* ______________________________________ *)
   [@@deriving variants, sexp_of]
 end
