49ba6d86d613a5e26372c707bc80120acfaf61c7
alanechang <alanechang@janestreet.com>
2024-02-05 16:38:12 -0500

    .

---
+++ debug_test.ml
@@ -0,0 +1,13 @@
+(* type _ g = MkG : 'a. 'a g
+
+   let g () = f (-#1l) #10l
+
+   let g () = f #1l #10l
+
+   let g () = f 1l 10l
+
+   let f x = match x with -1l | -2l -> true | _ -> false
+
+   let f x = match x with 1l | 2l -> true | _ -> false
+
+   let f x = match x with -#1l | -#2l -> true | _ -> false *)
